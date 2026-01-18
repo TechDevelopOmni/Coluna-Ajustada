6f0f1049c3c4f489f164fe472be556ecf5b2987e
TechDevelopOmni <146587539+TechDevelopOmni@users.noreply.github.com>
2026-01-18 20:42:36 -0300

    Add ColorBends background to main view

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
-import DotGrid from './DotGrid'
+import ColorBends from './ColorBends'
 
 const MainView: React.FC = () => {
     return (
-        <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-            <DotGrid
-                dotSize={10}
-                gap={15}
-                baseColor="#5227FF"
-                activeColor="#5227FF"
-                proximity={120}
-                shockRadius={250}
-                shockStrength={5}
-                resistance={750}
-                returnDuration={1.5}
-            />
+        <div style={{ position: 'relative', width: '100%', minHeight: '100vh', overflow: 'hidden' }}>
+            <div style={{ position: 'absolute', inset: 0 }}>
+                <ColorBends
+                    colors={['#ff5c7a', '#8a5cff', '#00ffd1']}
+                    rotation={0}
+                    speed={0.2}
+                    scale={1}
+                    frequency={1}
+                    warpStrength={1}
+                    mouseInfluence={1}
+                    parallax={0.5}
+                    noise={0.1}
+                    transparent
+                    autoRotate={0}
+                />
+            </div>
         </div>
     )
 }
